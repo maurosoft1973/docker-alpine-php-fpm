647fbe29bd7192613935ad9b33741017d720cbc2
Mauro Cardillo <gita@maurosoft.com>
2021-03-23 12:51:03 +0100

    Update

--- build-container.sh
+++ build-container.sh
@@ -4,26 +4,15 @@
 #
 source ./.env
 
-#The version of PHP is 
-declare -A PHP_VERSIONS
-PHP_VERSIONS["edge"]="7.4.16-r0"
-PHP_VERSIONS["3.13"]="7.4.15-r0"
-PHP_VERSIONS["3.12"]="7.3.27-r0"
-PHP_VERSIONS["3.11"]="7.3.22-r0"
-PHP_VERSIONS["3.10"]="7.3.14-r0"
-PHP_VERSIONS["3.9"]="7.2.33-r0"
-PHP_VERSIONS["3.8"]="7.2.26-r0"
-PHP_VERSIONS["3.7"]="7.1.33-r0"
-
 # Default values of arguments
-IMAGE=maurosoft1973/alpine-php-fpm:test
+IMAGE=maurosoft1973/alpine-php-fpm
+IMAGE_TAG=latest
 CONTAINER=alpine-php-fpm
 LC_ALL=it_IT.UTF-8
 TIMEZONE=Europe/Rome
-DEBUG=0
 IP=0.0.0.0
 PORT=7000
-WWW=/var/www
+WWW_DATA=$(pwd)
 WWW_USER=root
 WWW_USER_UID=0
 WWW_GROUP=www-data
@@ -32,36 +21,33 @@ PHP_POOL_MAX_CHILDREN=5
 PHP_POOL_START_SERVERS=1
 PHP_POOL_MIN_SPARE_SERVERS=1
 PHP_POOL_MAX_SPARE_SERVERS=3
+PHP_XDEBUG_ENABLED=1
 
 # Loop through arguments and process them
 for arg in "$@"
 do
     case $arg in
-        -ir=*|--image-release=*)
-        IMAGE_RELEASE="${arg#*=}"
+        -it=*|--image-tag=*)
+        IMAGE_TAG="${arg#*=}"
         shift # Remove
         ;;
-        -d=*|--debug=*)
-        DEBUG="${arg#*=}"
-        shift # Remove
-        ;;
-        -c=*|--container=*)
+        -cn=*|--container=*)
         CONTAINER="${arg#*=}"
         shift # Remove
         ;;
-        -l=*|--lc_all=*)
+        -cl=*|--lc_all=*)
         LC_ALL="${arg#*=}"
         shift # Remove
         ;;
-        -t=*|--timezone=*)
+        -ct=*|--timezone=*)
         TIMEZONE="${arg#*=}"
         shift # Remove
         ;;
-        -i=*|--ip=*)
+        -ci=*|--ip=*)
         IP="${arg#*=}"
         shift # Remove
         ;;
-        -p=*|--port=*)
+        -cp=*|--port=*)
         PORT="${arg#*=}"
         shift # Remove
         ;;
@@ -88,13 +74,12 @@ do
         -h|--help)
         echo -e "usage "
         echo -e "$0 "
-        echo -e "  -ir=|--image-release -> ${IMAGE}:${IMAGE_RELEASE} (image with release)"
-        echo -e "  -d=|--debug -> ${DEBUG} debug mode"
-        echo -e "  -c=|--container -> ${CONTAINER} name of container"
-        echo -e "  -l=|--lc_all -> ${LC_ALL} locale"
-        echo -e "  -t=|--timezone -> ${TIMEZONE} timezone"
-        echo -e "  -i=|--ip -> ${IP} (address ip listen)"
-        echo -e "  -p=|--port -> ${PORT} (port listen)"
+        echo -e "  -it=|--image-tag -> ${IMAGE}:${IMAGE_TAG} (image with tag)"
+        echo -e "  -cn=|--container -> ${CONTAINER} (container name)"
+        echo -e "  -cl=|--lc_all -> ${LC_ALL} (container locale)"
+        echo -e "  -ct=|--timezone -> ${TIMEZONE} (container timezone)"
+        echo -e "  -ci=|--ip -> ${IP} (container ip)"
+        echo -e "  -cp=|--port -> ${PORT} (container port listen)"
         echo -e "  -wd=|--www_data -> ${WWW_DATA} (www data)"
         echo -e "  -wu=|--www_user -> ${WWW_USER} (www user)"
         echo -e "  -wui=|--www_user_id -> ${WWW_USER_UID} (www user uid)"
@@ -105,18 +90,17 @@ do
     esac
 done
 
-echo "# Image               : ${IMAGE}:${IMAGE_RELEASE}"
-echo "# Container Name      : ${CONTAINER}"
-echo "# Debug Mode          : ${DEBUG}"
-echo "# Locale              : ${LC_ALL}"
-echo "# Timezone            : ${TIMEZONE}"
-echo "# IP Listen           : $IP"
-echo "# Port Listen         : $PORT"
-echo "# WWW Data            : $WWW"
-echo "# WWW User            : $WWW_USER"
-echo "# WWW User UID        : $WWW_USER_UID"
-echo "# WWW Group           : $WWW_GROUP"
-echo "# WWW Group UID       : $WWW_GROUP_UID"
+echo "# Image                   : ${IMAGE}:${IMAGE_TAG}"
+echo "# Container Name          : ${CONTAINER}"
+echo "# Container Locale        : ${LC_ALL}"
+echo "# Container Timezone      : ${TIMEZONE}"
+echo "# Container IP            : $IP"
+echo "# Container Port          : $PORT"
+echo "# WWW Data                : $WWW_DATA"
+echo "# WWW User                : $WWW_USER"
+echo "# WWW User UID            : $WWW_USER_UID"
+echo "# WWW Group               : $WWW_GROUP"
+echo "# WWW Group UID           : $WWW_GROUP_UID"
 
 echo -e "Check if container ${CONTAINER} exist"
 CHECK=$(docker container ps -a -f "name=${CONTAINER}" | wc -l)
@@ -131,7 +115,7 @@ else
 fi
 
 echo -e "Create and run container"
-docker run -dit --name ${CONTAINER} -p ${IP}:${PORT}:${PORT} -v ${WWW}:/var/www -e LC_ALL=${LC_ALL} -e TIMEZONE=${TIMEZONE} -e DEBUG=${DEBUG} -e WWW_USER=${WWW_USER} -e WWW_USER_UID=${WWW_USER_UID} -e WWW_GROUP=${WWW_GROUP} -e WWW_GROUP_UID=${WWW_GROUP_UID} -e IP=${IP} -e PORT=${PORT} ${IMAGE}
+docker run -dit --name ${CONTAINER} -p ${IP}:${PORT}:${PORT} -v ${WWW_DATA}:/var/www -e LC_ALL=${LC_ALL} -e TIMEZONE=${TIMEZONE} -e WWW_USER=${WWW_USER} -e WWW_USER_UID=${WWW_USER_UID} -e WWW_GROUP=${WWW_GROUP} -e WWW_GROUP_UID=${WWW_GROUP_UID} -e IP=${IP} -e PORT=${PORT} ${IMAGE}:${IMAGE_TAG}
 
 echo -e "Sleep 5 second"
 sleep 5
@@ -142,10 +126,6 @@ echo -e "IP Address is: ${IP}";
 echo -e ""
 echo -e "Environment variable";
 docker exec -it ${CONTAINER} env
-
-echo -e ""
-echo -e "Test Locale (date)";
-docker exec -it ${CONTAINER} date
 
 echo -e ""
 echo -e "Check FCGI Connection"
